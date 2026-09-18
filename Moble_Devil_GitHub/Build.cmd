4c4c7a529c1cd577502e3fb3f27f4dd004721e09
장성찬 <102020513+zx234x@users.noreply.github.com>
2026-09-18 15:37:53 +0900

    Delete Moble_Devil_GitHub directory

--- Moble_Devil_GitHub/Build.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Build.ps1" %*
-if errorlevel 1 (
-    echo.
-    echo Build failed. See the error above.
-    pause
-    exit /b 1
-)
